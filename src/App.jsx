7608bcf04353b694c674af137dd3fa6b0ffc1060
Moisés <olimpiodev.tec@gmail.com>
2026-05-27 15:44:49 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Local from "./components/Local";
 import Nav from "./components/nav/Nav";
 import Overview from "./components/Overview";
+import Register from "./components/Register";
 import Speaker from "./components/Speaker";
 import Sponsors from "./components/sponsors/Sponsors";
 import Top from "./components/Top";
@@ -13,6 +15,8 @@ function App() {
       <Overview />
       <Speaker />
       <Sponsors />
+      <Register />
+      <Local />
     </>
   );
 }
